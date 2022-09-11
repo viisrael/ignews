d3b1aa5e3aa6aaf1837bd504d2f5115547d2e17d
Vially Israel <viallyisrael@hotmail.com>
2022-09-11 16:09:00 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,23 @@
+import {FaGithub} from 'react-icons/fa';
+import styles from './styles.module.scss';
+import {FiX} from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+export function SignInButton(){
+  const {data: session} = useSession();
+  
+  return session ? (
+    <button type="button"  className={styles.signInButton}
+    onClick={ () => signOut()}>
+      <FaGithub color="#04d361" />
+      Vially Israel
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button"  className={styles.signInButton}
+    onClick={ () => signIn('github') }>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  )
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,57 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
+import { GetStaticProps } from 'next';
+import { stripe } from '../services/stripe';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title> Início ig.news </title>
+      </Head>
+
+      <main className={ styles.contentContainer }>
+        <section className={ styles.hero  }>
+          <span>👏 Hey, welcome</span>
+          <h1>News abaout the <span>React</span> world</h1>
+          <p>
+            Get accesss to all the publications <br/>
+            <span>for { product.amount } month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LaSJQLdirp6wkS0UKAJFSce');
+
+const product = {
+  priceId: price.id,
+  amount: new Intl.NumberFormat('en-US', {
+    style:'currency',
+    currency:'USD',
+  }).format((price.unit_amount / 100)),
+
+}
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, //24hrs
+  }
+}
