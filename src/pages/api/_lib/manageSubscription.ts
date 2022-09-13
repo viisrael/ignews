1de4502989b8a37d3b1668694e3ab901b5a41cbe
Vially Israel <viallyisrael@hotmail.com>
2022-09-12 22:30:53 -0300

    finished cap 3, unit 2, folder 04, video 2

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,9 @@
+export async function saveSubscription(subscription: string, customerId: string){
+
+  //Buscar o usuário no banco do fauna com o customerId
+  console.log(customerId);
+
+
+  //Salvar os dados da subsciption no FaunaDB
+  console.log(subscription);
+}
